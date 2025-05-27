514b8487ddc1a70864104830426c6638609a6571
tetracup <alexsklbus@gmail.com>
2025-05-27 12:28:20 +0200

    fastapi, uvicorn, get/post tests

---
+++ app/main.py
@@ -0,0 +1,36 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import uuid4
+from datetime import datetime
+from typing import List, Dict
+
+#Define sent message 
+class MessageIn(BaseModel):
+    recipient: str
+    message: str
+
+#define an in memory storage for testing
+messages_db: List[Dict] = []
+
+
+#Create FastAPI app
+app = FastAPI()
+
+#Define get on root 
+@app.get("/")
+def root():
+    return {"message": "Message service is up and running."}
+
+#define post on messages
+@app.post("/messages")
+def submit_message(msg: MessageIn):
+    new_message = {
+        "id": str(uuid4()),
+        "recipient": msg.recipient,
+        "message": msg.message,
+        "timestamp": datetime.utcnow().isoformat(),
+        "read": False
+    }
+    messages_db.append(new_message)
+    return {"status": "Message stored", "id": new_message["id"]}
