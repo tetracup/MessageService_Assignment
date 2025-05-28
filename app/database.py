e062c868adb4326d2ac13e3d3ce98ca03440fe5e
tetracup <alexsklbus@gmail.com>
2025-05-28 13:25:44 +0200

    upped security

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-
-conn_string = "postgresql+asyncpg://postgres.pwuczqnnxstvxbfnwkls:vWuC7Zwsx%40BxZC%24Xp%25%245@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
-
+import os
+#conn_string = "postgresql+asyncpg://postgres.pwuczqnnxstvxbfnwkls:vWuC7Zwsx%40BxZC%24Xp%25%245@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
+conn_string = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(conn_string, echo=True, connect_args={"statement_cache_size": 0})
 
